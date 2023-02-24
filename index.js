8c693db20c3e08f33409dce8d28ca0c1e0deab8e
MithunPrabhu777 <prabhuofficial7@gmail.com>
2023-02-25 00:12:35 +0530

    commited changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.post(`${process.env.BASE_URL}/api/form`,(req,res) => {
+app.post(`/api/form`,(req,res) => {
     let data = req.body;
 
     let smtpTransport = nodemailer.createTransport({
